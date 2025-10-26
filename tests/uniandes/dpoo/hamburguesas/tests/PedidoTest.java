fba19464572113040a25941563d5ebaa26ce5a1f
andrevt310 <a.vargast@uniandes.edu.co>
2025-10-26 17:27:25 -0500

    Prueba

--- tests/uniandes/dpoo/hamburguesas/tests/PedidoTest.java
+++ tests/uniandes/dpoo/hamburguesas/tests/PedidoTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+//hola
+
 import uniandes.dpoo.hamburguesas.mundo.Pedido;
 
 public class PedidoTest {
