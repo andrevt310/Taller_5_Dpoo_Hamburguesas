1d7668d6ab75b32789ce9a0ecc34dd0d5e68fc33
andrevt310 <a.vargast@uniandes.edu.co>
2025-10-26 17:28:03 -0500

    Prueba

--- tests/uniandes/dpoo/hamburguesas/tests/PedidoTest.java
+++ tests/uniandes/dpoo/hamburguesas/tests/PedidoTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-//hola
+//pueba
 
 import uniandes.dpoo.hamburguesas.mundo.Pedido;
 
